95d7fe750c10ea8b5bf21e3a6d6f15725f32fc6b
Aniket Yadav <aniketyadav690@gmail.com>
2022-06-10 03:44:08 +0530

    form section implemented

--- js/app.js
+++ js/app.js
@@ -6,21 +6,42 @@
 
 eventListner();
 function eventListner(){
-    const ui = new UI(); 
+  const ui = new UI(); 
 
     window.addEventListener("load", function(){
-       ui.hidePreloader(); 
+      ui.hidePreloader(); 
     })
     
     //nav btn
     document.querySelector(".navBtn").addEventListener("click", function(){
-        ui.showNav();
+      ui.showNav();
     })
 
     //control the video
     document.querySelector(".video__switch").addEventListener("click", function(){
-        ui.videoControls();
+      ui.videoControls();
     })
+
+    //Submit the form
+    document.querySelector(".drink-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    const name = document.querySelector(".input-name").value;
+    const lastName = document.querySelector(".input-lastname").value;
+    const email = document.querySelector(".input-email").value;
+
+    let value = ui.checkEmpty(name, lastName, email);
+
+    if(value){ //his means when the customer has added the details
+     let customer = new Customer(name, lastName, email);
+     console.log(customer);
+     ui.addCustomer(customer);
+     ui.showFeedback("Customer added to the list", "success");
+     ui.clearFields();
+
+    }else{
+      ui.showFeedback("Some form values are empty", "error");
+    }
+  })
 }
 
 //Constructor function
@@ -47,6 +68,68 @@ UI.prototype.videoControls = function(){
  }else{
     btn.classList.remove("btnSlide");
     document.querySelector(".video__item").play();
- }
+  }
 }
+
+//checking the empty values over here
+UI.prototype.checkEmpty = function(name, lastName, email){
+  let result;
+
+  if(name === '' || lastName === '' || email === ''){
+    result = false;
+  }else{
+    result = true;
+  }
+  return result;
+}
+
+UI.prototype.showFeedback = function(text, type){
+  const feedback = document.querySelector(".drink-form__feedback");
+  if(type === "success"){
+    feedback.classList.add("success");
+    feedback.innerText = text;
+    this.removeAlert("success");
+
+  }else if(type === "error"){
+    //let feedback = document.querySelector(".drink-form__feedback");
+    feedback.classList.add('error');
+    feedback.innerText = text;
+    this.removeAlert('error');
+  }
+}
+//Remove Alert
+UI.prototype.removeAlert = function(type){
+  setTimeout(function(){
+    document.querySelector(".drink-form__feedback").classList.remove(type);
+  }, 3000)
+}
+
+UI.prototype.addCustomer = function(customer){
+  const images = [1, 2, 3, 4, 5];
+  let random = Math.floor(Math.random() * images.length);
+  //console.log(random);
+  const div = document.createElement("div");
+  div.classList.add("person");
+  div.innerHTML = `<img src="img/person-${random}.jpeg" alt="person" class="person__thumbnail">
+  <h4 class="person__name">${customer.name}</h4>
+  <h4 class="person__last-name">${customer.lastName}</h4>`
+  document.querySelector(".drink-card__list").appendChild(div);
+}
+
+//clear fields
+UI.prototype.clearFields = function(){
+    document.querySelector(".input-name").value = '';
+    document.querySelector(".input-lastname").value = '';
+    document.querySelector(".input-email").value = '';
+}
+
+
+
+function Customer(name, lastName, email){
+  this.name = name;
+  this.lastName = lastName;
+  this.email = email;
+}
+
+//Work section untill next time
 
